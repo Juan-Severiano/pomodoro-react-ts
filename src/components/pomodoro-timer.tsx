fcf7ee30ff654841494b613676bada74ded048e9
Francisco Juan <juansoussev@gmail.com>
2024-03-03 23:17:35 -0300

    [fix] creating the cycles counter

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react"
 import { useInterval } from "../hooks/use-interval";
 import { Button } from "./button";
 import { Timer } from "./timer";
+import bellStart from '../assets/sounds/bell-start.mp3';
+import bellFinish from '../assets/sounds/bell-finish.mp3';
+
+const audioStartWorking = new Audio(bellStart);
+const audioStopWorking = new Audio(bellFinish);
 
 interface Props {
   pomodoroTime: number;
@@ -15,11 +20,22 @@ export function PomodoroTimer(props: Props): JSX.Element {
   const [timeConting, setTimeConting] = useState(false);
   const [working, setWorking] = useState(false);
   const [resting, setResting] = useState(false);
+  const [cycles, setCycles] = useState(new Array(props.cycles -1).fill(true))
 
   useEffect(() => {
     if (working) document.body.classList.add('working');
     if (resting) document.body.classList.remove('working');
-  }, [working, resting])
+
+    if (mainTime > 0) return
+
+    if (working && cycles.length > 0) {
+      configureRest(false);
+      cycles.pop()
+    } else if (working && cycles.length <= 0) {
+      configureRest(false);
+      setCycles(new Array(props.cycles -1).fill(true))
+    }
+  }, [working, resting, mainTime, cycles, props.cycles])
 
   useInterval(() => {
     setMainTime(mainTime - 1)
@@ -30,13 +46,14 @@ export function PomodoroTimer(props: Props): JSX.Element {
     setWorking(true)
     setResting(false);
     setMainTime(props.pomodoroTime)
+    audioStartWorking.play()
   }
 
   const configureRest = (long: boolean) => {
     setTimeConting(true);
     setWorking(false);
     setResting(true);
-
+    audioStopWorking.play()
     if (long) {
       setMainTime(props.longRestTime)
     } else {
