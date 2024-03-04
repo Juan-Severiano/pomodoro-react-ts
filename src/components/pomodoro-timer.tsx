0e1889a575cf5914a49a1dd97b29dc6abb120cce
Francisco Juan <juansoussev@gmail.com>
2024-03-03 22:46:24 -0300

    [feat] working button created

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useInterval } from "../hooks/use-interval";
 import { Button } from "./button";
 import { Timer } from "./timer";
@@ -12,17 +12,28 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeConting, setTimeConting] = useState(false);
+  const [working, setWorking] = useState(false);
+
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working])
 
   useInterval(() => {
     setMainTime(mainTime - 1)
-  }, 1000)
+  }, timeConting ? 1000 : null)
+
+  const configureWork = () => {
+    setTimeConting(true);
+    setWorking(true)
+  }
 
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
-        <Button text='Say hello' onClick={() => console.log('Hi')} />
+        <Button text='Work' onClick={() => configureWork()} />
         <Button text='Say hello' onClick={() => console.log('Hi')} />
         <Button text='Say hello' onClick={() => console.log('Hi')} />
         <Button text='Say hello' onClick={() => console.log('Hi')} />
